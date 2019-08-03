1ce3470f423088f518c4bd0e1af7296c8dd07c17
zhaopeng <772717607@qq.com>
2019-08-03 09:57:28 +0800

    请求拦截器 和响应拦截器

--- src/api/ajax.js
+++ src/api/ajax.js
@@ -3,5 +3,37 @@
 */
 
 import axios from 'axios'
+import qs from 'qs'
+
+//对所有的post请求 data 的对象进行字符串化  Interceptors 请求拦截器
+
+//添加请求拦截器 让 post请求的请求体的格式转化为 urlencided格式 a:1&b:1形式
+axios.interceptors.request.use(function (config) {
+
+    //得到请求方式和请求体数据
+    const {method,data}=config
+    //处理post请求，将data对象转换成query参数格式字符串
+    if (method.toLowerCase()==='post'&& typeof data==='object') {
+       config.data =  qs.stringify(data)
+    }
+    return config;
+  });
+// Add a response interceptor 添加响应的拦截器，在请求返回值后，且在我们指定的请求回调之前执行
+axios.interceptors.response.use(function (response) {
+  // Do something with response data  我们想用的数据就是response.data
+ 
+  return response.data;//此返回结果就会交给我们指定的请求响应的回调 ---->index.js 中的请求回调
+
+}, function (error) {  //  统一处理所有请求异常错误，不用再去具体请求中处理
+  /*  Do something with response error
+  return Promise.reject(error);
+   如果出现错误   会先执行 拦截器的 回调，然后在执行请求失败的回调，现在想要在拦截器里直接解决错误! 不在去执行请求失败的回调
+  请求的回调是依据拦截器的Promise回调返回结果判断  是否执行，如果是reject 请求的回调就会执行
+   如果  返回一个pending状态的promise，就会中断promise链
+  */
+  alert('请求出错'+error.message)
+  return new Promise(()=>{}) // new 之后 初始化就是pending状态。
+});
+
 
 export default axios
--- src/api/index.js
+++ src/api/index.js
@@ -2,6 +2,7 @@
 包含应用中 所有请求接口的函数 --->接口请求函数
       在此处封装  谁想发请求就引入 然后调用函数即可。
 */
+import qs from 'qs'
 import  ajax from './ajax'
 
 //const BASE = 'http://localhost:5000'
@@ -9,21 +10,36 @@ import  ajax from './ajax'
 //定义请求登陆--- post 请求方式
 // 调用时将实参数据 赋值 形参变量
 export  function reqLogin(username,password) {
-    ajax({ //配置对象
-    
+//获取axios post请求返回结果 需要return，返回promise对象，调用函数之后才能  .then  .catch
+return ajax({ //配置对象  
         method:'post',
+        //url:'http://localhost:3000/login'
         url:'/login', //配置完代理  就不存在跨域问题了
-        data:{
+        data:{  //data是对象的时候 默认使用json格式的请求体携带参数
             username,
             password
-        }
-
+        } 
+       //  username=admin&password=admin 
+       // data:qs.stringify({username,password})   // 不想用json格式  想要 形成 urlencoded 形式  地址栏  a:1&b:1形式
     })
 } 
-
-
-
+/* 
 const n='admin'
 const p = 'admin'
+reqLogin(n,p).then((res)=>{
+//得到返回的数据
+ const result = res.data
+    console.log(result,'请求成功了');   
+},(err)=>{console.log(err);
+})  */
 
-reqLogin(n,p)
+//每次想要 获取数据都需要 .data  很烦 进一步改进  用响应拦截器 看ajax.js
+const n='admin'
+const p = 'admin2'
+reqLogin(n,p).then((result)=>{
+//得到返回的数据
+    console.log(result,'请求成功了');   
+
+   // 将res 请求错误时  返回的promise对象为 pending状态 次回调就不会执行了
+},(err)=>{console.log(err.message); //应用响应拦截器，解决 。message
+}) 
