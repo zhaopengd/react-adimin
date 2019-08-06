33e8cc6ba48cf614d3ce26777cf322d71cabeafd
zhaopeng <772717607@qq.com>
2019-08-06 18:18:31 +0800

    heade 静态页面

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -4,7 +4,18 @@ export default class Header extends Component {
     render() {
         return (
             <div className='header'>
-                Header
+                <div className='header-top'>
+                    欢迎，admin &nbsp;&nbsp;
+                    <a href="###">退出</a>
+                </div>
+                <div className='header-bottom'>
+                    <div className='header-bottom-left'>角色管理</div>
+                    <div className='header-bottom-right'>
+                        <span>2019-1-1*******</span>
+                        <img src="http://api.map.baidu.com/images/weather/day/duoyun.png" alt=""/>
+                        <span>晴</span>
+                    </div>
+                </div>
             </div>
         )
     }
